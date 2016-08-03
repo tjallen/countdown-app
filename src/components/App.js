5425b1e8247227ab4f033ab201239e2b7f739345
Thom Allen <thomwork@gmail.com>
2016-08-03 12:38:23 +0100

    Add audioElement, beginnings of custom audio controls, destructured render vars
    +rando placeholders

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 // css modules
 import styles from './App.scss';
 
+// audio files
+import chime from '../files/chime.mp3';
+
 export default class App extends Component {
   constructor() {
     super();
@@ -11,15 +14,26 @@ export default class App extends Component {
       paused: false,
       stopped: true,
       targetSeconds: 3,
+      chime,
+      volume: '0.1',
+      looping: true,
+      muted: false,
     };
     this.tick = this.tick.bind(this);
     this.onTimerStart = this.onTimerStart.bind(this);
     this.onTimerPause = this.onTimerPause.bind(this);
     this.onTimerClear = this.onTimerClear.bind(this);
+    this.onTimerRestart = this.onTimerRestart.bind(this);
+    this.toggleChimeMute = this.toggleChimeMute.bind(this);
+    this.onVolumeInputChange = this.onVolumeInputChange.bind(this);
   }
   componentDidMount() {
     // define timerInterval but don't set it yet
     this.timerInterval = null;
+    // initialize audio element
+    this.audioElement.volume = this.state.volume;
+    // audio element debug info
+    console.log(`muted: ${this.audioElement.muted}, volume: ${this.audioElement.volume}`);
   }
   componentWillUnmount() {
     clearInterval(this.timerInterval);
@@ -52,6 +66,25 @@ export default class App extends Component {
       formattedTime: this.formatTime(0),
     }, clearInterval(this.timerInterval));
   }
+  onTimerRestart() {
+    this.onTimerClear();
+    this.onTimerStart();
+  }
+  onVolumeInputChange(event) {
+    console.log(event.target.value);
+    this.setState({
+      volume: event.target.value,
+    });
+  }
+  toggleChimeMute() {
+    this.audioElement.muted = !this.audioElement.muted;
+    this.setState({
+      muted: !this.audioElement.muted,
+    });
+  }
+  toggleCountdown() {
+    // toggle count up || count down
+  }
   // tick method run by interval to update timer once a second
   tick(timerStartDate) {
     // prepare a callback for when the target second is about to be reached
@@ -68,6 +101,7 @@ export default class App extends Component {
   // fire the chime, message etc when target seconds is arrived at
   targetSecondsReached() {
     this.onTimerPause();
+    this.audioElement.play();
     console.log('target seconds reached!');
   }
   // take time in seconds and format to hh:mm:ss
@@ -91,8 +125,8 @@ export default class App extends Component {
     return paddedNum;
   }
   render() {
-    const isStopped = this.state.stopped;
-    const isPaused = this.state.paused;
+    // destructure this.state into some helper variables for readability
+    const { muted: isMuted, stopped: isStopped, paused: isPaused } = this.state;
     const isPlaying = this.state.seconds > 0 && !isPaused;
     let startButtonText;
     if (isPaused) {
@@ -100,9 +134,18 @@ export default class App extends Component {
     } else {
       startButtonText = 'Start';
     }
+    let muteButtonText;
+    if (isMuted) {
+      muteButtonText = 'Unmute';
+    } else {
+      muteButtonText = 'Mute';
+    }
+    // dbg
+    console.log(isMuted, isStopped, isPaused, isPlaying);
     return (
       <div className={styles.container}>
         <h2>{this.state.formattedTime}</h2>
+        <p>Timer controls</p>
         <button
           onClick={this.onTimerStart}
           disabled={isPlaying}
@@ -114,13 +157,34 @@ export default class App extends Component {
         <button
           onClick={this.onTimerClear}
           disabled={isStopped}
-        >Reset</button>
-        <ul>
-          <li>Hourly</li>
-          <li>Pomorodo</li>
-          <li>Custom</li>
-          <li>Chime</li>
-        </ul>
+        >Clear</button>
+        <button
+          onClick={this.onTimerRestart}
+          disabled={isStopped}
+        >Restart</button>
+        <br />
+        <p>Timer type</p>
+        <button>Count down</button>
+        <button>Count up</button>
+        <button>Pomorodo</button>
+        <button>Toggle loop</button>
+        <p>chime controls</p>
+        <button onClick={this.toggleChimeMute}>{muteButtonText}</button>
+        Volume: <input
+          type="range"
+          min="0"
+          max="1"
+          step="0.1"
+          value={this.state.volume}
+          onChange={this.onVolumeInputChange}
+        ></input>
+        <audio
+          ref={(c) => (this.audioElement = c)}
+        >
+          html5 <code>audio</code> element isn't supported by your browser, dayum.
+          <source src={this.state.chime} type="audio/mpeg">
+          </source>
+        </audio>
       </div>
     );
   }
