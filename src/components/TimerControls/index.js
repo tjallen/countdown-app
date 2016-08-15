11559f7296bd3060c5291cf6c4a929c0da5f8bf9
Thom Allen <thomwork@gmail.com>
2016-08-15 20:45:50 +0100

    Start extracting buttons into TimerButton component

--- src/components/TimerControls/index.js
+++ src/components/TimerControls/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
+import TimerButton from '../TimerButton';
+import play from '../../icons/ic_play_arrow_24px.svg';
+import pause from '../../icons/ic_pause_24px.svg';
+import clear from '../../icons/ic_delete_24px.svg';
+import restart from '../../icons/ic_replay_24px.svg';
+
 const TimerControls = (props) =>
   <div>
+    <TimerButton
+      action={props.onTimerStart}
+      glyph={play}
+    />
+    <TimerButton
+      action={props.onTimerPause}
+      glyph={pause}
+    />
+    <TimerButton
+      action={props.onTimerClear}
+      glyph={clear}
+    />
+    <TimerButton
+      action={props.onTimerRestart}
+      glyph={restart}
+    />
     <button
       onClick={props.onTimerStart}
       disabled={props.isPlaying}
