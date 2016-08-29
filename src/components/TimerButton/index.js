6c7c338af35cef0ecdac632dc961fc98caa4670b
Thom Allen <thomwork@gmail.com>
2016-08-29 22:26:37 +0100

    Add classnames module + some :art:

--- src/components/TimerButton/index.js
+++ src/components/TimerButton/index.js
@@ -1,16 +1,32 @@
 import React, { PropTypes } from 'react';
 import Icon from '../Icon';
 
-const TimerButton = ({ action, glyph, title }) =>
-  <button
-    onClick={action}
-    title={title}
-  >
-    <Icon glyph={glyph} />
-  </button>;
+import classNames from 'classnames/bind';
+import styles from './TimerButton.scss';
+const cx = classNames.bind(styles);
+
+const TimerButton = ({ action, glyph, title }) => {
+  let className = cx({
+    btn: true,
+    playpause: title === 'Play' || title === 'Pause',
+    play: title === 'Play',
+    pause: title === 'Pause',
+  });
+  return (
+    <button
+      onClick={action}
+      title={title}
+      className={className}
+    >
+      <Icon glyph={glyph} />
+    </button>
+  );
+};
+
 TimerButton.propTypes = {
   action: PropTypes.func.isRequired,
   glyph: PropTypes.string,
   title: PropTypes.string,
+  className: PropTypes.string,
 };
 export default TimerButton;
