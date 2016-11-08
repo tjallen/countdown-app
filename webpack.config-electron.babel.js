35114013dff679cf6cee5d5f32b4ac6b518644cf
Thom Allen <thomwork@gmail.com>
2016-11-08 19:00:12 +0000

    Add electron-packager and electron:build script, update ignores, package.jsons

--- webpack.config-electron.babel.js
+++ webpack.config-electron.babel.js
@@ -44,7 +44,7 @@ module.exports = {
     ], {
       verbose: true,
       dry: false,
-      exclude: ['.gitignore', 'main.js', 'index-electron.html'],
+      exclude: ['.gitignore', 'main.js', 'index-electron.html', 'package.json'],
     }),
 /*    new CopyWebpackPlugin([
       { from: 'src/images', to: PATHS.distImages },
